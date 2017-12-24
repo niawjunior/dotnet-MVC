a845eac6ff0ec74c01e93e674b1f0e2c1e884aa0
niawjunior <pasupol@dome.cloud>
2017-12-24 16:40:56 +0700

    Controller and View

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //public string Index()
+        //{
+        //    return "This string is from MVC Controller";
+        //}
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -7,14 +7,11 @@ namespace DotNetCoreMVC
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-           
+          services.AddMvc(); 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
@@ -22,6 +19,13 @@ namespace DotNetCoreMVC
             defaultFilesOptions.DefaultFileNames.Add("home.html");
             app.UseDefaultFiles(defaultFilesOptions);
             app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Employee}/{action=Index}/{id?}");
+            });
         }
     }
 }
